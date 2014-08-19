5df81c83c0e7a5a428a577c673f6eb5fe2788bbf
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2014-08-20 01:37:48 +0200

    Code style fixes

--- services/geo_location.go
+++ services/geo_location.go
@@ -1,19 +1,22 @@
 package services
 
 import (
-	"github.com/oschwald/geoip2-golang"
 	"net"
+
+	geoip2 "github.com/oschwald/geoip2-golang"
 )
 
+// GeoLocation ...
 type GeoLocation struct {
 	IP       string
 	Location *geoip2.City
 }
 
+// NewGeoLocation ...
 func NewGeoLocation(IP string) (*GeoLocation, error) {
 	gl := &GeoLocation{IP: IP}
 
-	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	db, err := geoip2.Open("data/GeoLite2-City.mmdb")
 	defer db.Close()
 
 	if err != nil {
@@ -21,7 +24,6 @@ func NewGeoLocation(IP string) (*GeoLocation, error) {
 	}
 
 	record, err := db.City(net.ParseIP(gl.IP))
-
 	if err != nil {
 		return nil, err
 	}
--- services/mongo.go
+++ services/mongo.go
@@ -1,26 +1,29 @@
 package services
 
 import (
-    "labix.org/v2/mgo"
-    "github.com/daviddengcn/go-colortext"
-     "log"
+	"github.com/fatih/color"
+	"labix.org/v2/mgo"
 )
 
-var MongoPool Pool = Pool{}
+// MongoPool ...
+var MongoPool = Pool{}
 
+// Mongo ...
 type Mongo struct {
-    connectionString string
-    databaseName string
+	connectionString string
+	databaseName     string
 }
 
+// NewMongo ...
 func NewMongo(connectionString, databaseName string) (mongo *Mongo) {
-    mongo = new(Mongo)
-    mongo.connectionString = connectionString
-    mongo.databaseName = databaseName
+	mongo = new(Mongo)
+	mongo.connectionString = connectionString
+	mongo.databaseName = databaseName
 
-    return
+	return
 }
 
+// Connect ...
 func (m *Mongo) Connect() {
 	var err error
 
@@ -28,23 +31,46 @@ func (m *Mongo) Connect() {
 		panic(err)
 	}
 
-    ct.ChangeColor(ct.Yellow, false, ct.None, false)
-	log.Println("Connected to MongoDB.")
-
+	color.Green("Connected to MongoDB.")
 	MongoPool.Database = MongoPool.Session.DB(m.databaseName)
+	MongoPool.AddIndexes()
 }
 
+// AddIndexes @TODO fix that and put this code to the repositores
+func (mp *Pool) AddIndexes() {
+	index1 := mgo.Index{
+		Key:        []string{"first_action_at_bucket"},
+		Unique:     false,
+		Background: true,
+	}
+
+	index2 := mgo.Index{
+		Key:        []string{"last_action_at_bucket"},
+		Unique:     false,
+		Background: true,
+	}
+
+	err := mp.Collection("visit").EnsureIndex(index1)
+	err = mp.Collection("visit").EnsureIndex(index2)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Disconnect ...
 func (m *Mongo) Disconnect() {
-    MongoPool.Session.Close()
+	MongoPool.Session.Close()
 }
 
+// Pool ...
 type Pool struct {
-    Session *mgo.Session
-    Database *mgo.Database
+	Session  *mgo.Session
+	Database *mgo.Database
 }
 
+// Collection ...
 func (p *Pool) Collection(name string) (collection *mgo.Collection) {
-    collection = p.Database.C(name)
+	collection = p.Database.C(name)
 
-    return
+	return
 }
